08b20d32c674d30eedc31aa0cf022b75da6b20bf
athowes <adamthowes@gmail.com>
2025-07-09 21:19:29 +0100

    Move into set_riskiness() function

--- R/setup.R
+++ R/setup.R
@@ -11,14 +11,14 @@ init_counts <- function(N, prop_E = 0.005) {
 
 # Takes the basic parameters then builds out the complete parameter list
 expand_parameters <- function(
-    archetype,
-    coverage_type,
-    coverage,
-    riskiness,
-    efficacy,
-    iteration,
-    scenario,
-    id
+  archetype,
+  coverage_type,
+  coverage,
+  riskiness,
+  efficacy,
+  iteration,
+  scenario,
+  id
 ) {
   overrides <- inject(list(
     human_population = config$human_population,
@@ -45,38 +45,43 @@ expand_parameters <- function(
   }
 
   if (riskiness == "setting_specific_riskiness") {
-    param <- param |>
-      set_setting_specific_riskiness(
-        setting = "school",
-        mean = 0,
-        sd = 0.3544,
-        min = 1 / sqrt(4.75),
-        max = sqrt(4.75)
-      ) |>
-      set_setting_specific_riskiness(
-        setting = "workplace",
-        mean = 0,
-        sd = 0.5072,
-        min = 1 / sqrt(6.35),
-        max = sqrt(6.35)
-      ) |>
-      set_setting_specific_riskiness(
-        setting = "household",
-        mean = 0,
-        sd = 0.0871,
-        min = 1 / sqrt(2.5),
-        max = sqrt(2.5)
-      ) |>
-      set_setting_specific_riskiness(
-        setting = "leisure",
-        mean = 0,
-        sd = 0.4278,
-        min = 1 / sqrt(5.5),
-        max = sqrt(5.5)
-      )
+    param <- set_riskiness(param)
   }
 
   param$iteration <- iteration
   param$id <- id
   return(param)
+}
+
+# Could put these in config
+set_riskiness <- function(df) {
+  df |>
+    set_setting_specific_riskiness(
+      setting = "school",
+      mean = 0,
+      sd = 0.3544,
+      min = 1 / sqrt(4.75),
+      max = sqrt(4.75)
+    ) |>
+    set_setting_specific_riskiness(
+      setting = "workplace",
+      mean = 0,
+      sd = 0.5072,
+      min = 1 / sqrt(6.35),
+      max = sqrt(6.35)
+    ) |>
+    set_setting_specific_riskiness(
+      setting = "household",
+      mean = 0,
+      sd = 0.0871,
+      min = 1 / sqrt(2.5),
+      max = sqrt(2.5)
+    ) |>
+    set_setting_specific_riskiness(
+      setting = "leisure",
+      mean = 0,
+      sd = 0.4278,
+      min = 1 / sqrt(5.5),
+      max = sqrt(5.5)
+    )
 }
