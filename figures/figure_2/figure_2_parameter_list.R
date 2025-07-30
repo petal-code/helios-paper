d4cc043f6cf495b1348e1a324c03a62167bb0470
Geetha Jeyapragasan <146225268+geethaj1@users.noreply.github.com>
2025-07-30 12:39:42 -0400

    Update figures/figure_2/figure_2_parameter_list.R
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- figures/figure_2/figure_2_parameter_list.R
+++ figures/figure_2/figure_2_parameter_list.R
@@ -122,44 +122,6 @@ for (i in 1:nrow(simulations_to_run)) {
         seed = simulations_to_run$seed[i]
       )
     )
-  } 
-  }
-  
-  # UVC Parameters
-  if (simulations_to_run$coverage[i] > 0) {
-    parameter_lists[[i]] <- parameter_lists[[i]] %>%
-      set_uvc(
-        setting = "joint",
-        coverage = simulations_to_run$coverage[i],
-        coverage_target = "square_footage",
-        coverage_type = simulations_to_run$coverage_type[i],
-        efficacy = simulations_to_run$efficacy[i],
-        timestep = 0
-      )
-  }
-  
-  if (simulations_to_run$riskiness[i] == "setting_specific_riskiness") {
-    parameter_lists[[i]] <- parameter_lists[[i]] %>%
-      set_setting_specific_riskiness(
-        setting = "school",
-        mean = 0, sd = 0.3544,
-        min = 1 / sqrt(4.75), max = sqrt(4.75)
-      ) %>%
-      set_setting_specific_riskiness(
-        setting = "workplace", 
-        mean = 0, sd = 0.5072,
-        min = 1 / sqrt(6.35), max = sqrt(6.35)
-      ) %>%
-      set_setting_specific_riskiness(
-        setting = "household",
-        mean = 0, sd = 0.0871,
-        min = 1 / sqrt(2.5), max = sqrt(2.5)
-      ) %>%
-      set_setting_specific_riskiness(
-        setting = "leisure",
-        mean = 0, sd = 0.4278,
-        min = 1 / sqrt(5.5), max = sqrt(5.5)
-      )
   }
   
   parameter_lists[[i]]$simulation_id <- simulations_to_run$ID[i]
