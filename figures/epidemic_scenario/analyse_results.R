21e886684b92fb68e1d79a918bdca035b421f9d5
athowes <adamthowes@gmail.com>
2025-08-12 22:02:07 +0100

    Calculate metrics for simulations

--- figures/epidemic_scenario/analyse_results.R
+++ figures/epidemic_scenario/analyse_results.R
@@ -3,19 +3,35 @@ source(here::here("packages.R"))
 version <- "figure_4_epidemic_simulations"
 files <- list.files(path = file.path("figures", "epidemic_scenario", version), full.names = TRUE)
 
-# archetype
-# coverage_type
-# coverage
-# riskiness
-# efficacy
-# iteration
-# scenario
-# id
-# seed
+get_results <- function(sim) {
+  df <- pivot_to_long(sim$simulation)
+  results <- list(
+    "epidemic_final_size" = epidemic_final_size(df),
+    "time_to_peak_infections" = time_to_peak_infections(df, dt = sim$parameters$dt),
+    "peak_daily_incidence" = peak_daily_incidence(df, dt = sim$parameters$dt),
+    "id" = sim$parameters$id
+  )
+  return(results)
+}
 
-sim <- readRDS(files[1])
+results <- purrr::map(files, readRDS) |>
+  purrr::map_dfr(get_results) 
 
-df <- pivot_to_long(sim$simulation)
-epidemic_final_size <- epidemic_final_size(df)
-time_to_peak_infections <- time_to_peak_infections(df, dt = sim$parameters$dt)
-peak_daily_incidence <- peak_daily_incidence(df, dt = sim$parameters$dt)
+# simulation_settings obtained from previous script. Possible we want to save this with the simulations
+results <- arrange(results, id) |>
+  left_join(
+    simulation_settings,
+    by = "id"
+  )
+  
+results_long <- results |>
+  tidyr::pivot_longer(
+    cols = c("epidemic_final_size", "time_to_peak_infections", "peak_daily_incidence"),
+    names_to = "metric",
+    values_to = "value"
+  )
+
+ggplot(results_long, aes(x = id, y = value)) +
+  geom_point() +
+  facet_wrap(~metric, scales = "free") +
+  theme_minimal()
