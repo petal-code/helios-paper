1d4d3be0f6f9b3a05f4409fe109d959476d266b1
athowes <adamthowes@gmail.com>
2025-08-13 17:54:59 +0100

    Format

--- figures/epidemic_scenario/analyse_results.R
+++ figures/epidemic_scenario/analyse_results.R
@@ -1,13 +1,19 @@
 source(here::here("packages.R"))
 
 version <- "figure_4_epidemic_simulations"
-files <- list.files(path = file.path("figures", "epidemic_scenario", version), full.names = TRUE)
+files <- list.files(
+  path = file.path("figures", "epidemic_scenario", version),
+  full.names = TRUE
+)
 
 get_results <- function(sim) {
   df <- pivot_to_long(sim$simulation)
   results <- list(
     "epidemic_final_size" = epidemic_final_size(df),
-    "time_to_peak_infections" = time_to_peak_infections(df, dt = sim$parameters$dt),
+    "time_to_peak_infections" = time_to_peak_infections(
+      df,
+      dt = sim$parameters$dt
+    ),
     "peak_daily_incidence" = peak_daily_incidence(df, dt = sim$parameters$dt),
     "id" = sim$parameters$id
   )
@@ -15,7 +21,7 @@ get_results <- function(sim) {
 }
 
 results <- purrr::map(files, readRDS) |>
-  purrr::map_dfr(get_results) 
+  purrr::map_dfr(get_results)
 
 # simulation_settings obtained from previous script. Possible we want to save this with the simulations
 results <- arrange(results, id) |>
@@ -23,10 +29,14 @@ results <- arrange(results, id) |>
     simulation_settings,
     by = "id"
   )
-  
+
 results_long <- results |>
   tidyr::pivot_longer(
-    cols = c("epidemic_final_size", "time_to_peak_infections", "peak_daily_incidence"),
+    cols = c(
+      "epidemic_final_size",
+      "time_to_peak_infections",
+      "peak_daily_incidence"
+    ),
     names_to = "metric",
     values_to = "value"
   )
@@ -53,7 +63,11 @@ results_summary |>
   filter(metric == "epidemic_final_size") |>
   ggplot(aes(x = coverage)) +
   geom_pointrange(
-    aes(y = mean_value, ymin = mean_value - sd_value, ymax = mean_value + sd_value),
+    aes(
+      y = mean_value,
+      ymin = mean_value - sd_value,
+      ymax = mean_value + sd_value
+    ),
     position = position_dodge(width = 0.1)
   ) +
   facet_grid(coverage_type ~ efficacy) +
