b419f6d8b525f735bceda27ab71d06ca98f93844
Thomas Brewer <trb216@dide.local>
2025-08-15 10:55:16 +0100

    Fixed conflict in run.R

--- R/run.R
+++ R/run.R
@@ -1,26 +1,26 @@
 run_simulation_hipercow <- function(
-  parameters,
-  file_save = FALSE,
-  directory = NULL
+    parameters,
+    file_save = FALSE,
+    directory = NULL
 ) {
   # Terminate operation if no directory provided:
   if (is.null(directory)) {
     stop("Must provide a directory for output storage")
   }
-
+  
   # Run the simulation:
   s <- helios::run_simulation(parameters_list = parameters)
-
+  
   # Append the simulation identifier to the simulation outputs:
   s$id <- parameters$id
   s$iteration <- parameters$iteration
   s$scenario <- parameters$scenario
-
+  
   # Store parameter list and simulated outputs in a single list for returning:
   output <- list()
   output$parameters <- parameters
   output$simulation <- s
-
+  
   # Get the date/time stamp:
   time_stamp <- format(Sys.time(), "%Y%m%d_%H%M")
   # TODO: Generalise the saveRDS call for use across figures
@@ -42,6 +42,6 @@ run_simulation_hipercow <- function(
       )
     )
   }
-
+  
   return(output)
 }
